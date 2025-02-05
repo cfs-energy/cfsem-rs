799a011d3102af0c59008c91b8f96f777d36ea2e
James Logan <jlogan@cfs.energy>
2025-02-05 09:35:59 -0500

    autoformat

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -5,7 +5,7 @@ pub mod gradshafranov;
 pub mod linear_filament;
 pub mod point_source;
 
-#[doc(hidden)]  // Might make breaking changes soon
+#[doc(hidden)] // Might make breaking changes soon
 pub mod mesh_filament;
 
 pub use circular_filament::{flux_circular_filament, flux_density_circular_filament};
